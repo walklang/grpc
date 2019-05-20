60e8e14799c157958bee807ae58baa0b9c2be6b3
suhao <638781@qq.com>
2019-05-20 17:24:47 +0800

    add vs2015 solution.

---
+++ build/x64/third_party/benchmark/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: D:/rpc/grpc/third_party/benchmark
+# Build directory: D:/rpc/build/x64/third_party/benchmark
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("src")
+subdirs("test")
---
+++ build/x86/third_party/zlib/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: D:/rpc/grpc/third_party/zlib
+# Build directory: D:/rpc/build/x86/third_party/zlib
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(example "example")
+set_tests_properties(example PROPERTIES  _BACKTRACE_TRIPLES "D:/rpc/grpc/third_party/zlib/CMakeLists.txt;235;add_test;D:/rpc/grpc/third_party/zlib/CMakeLists.txt;0;")
